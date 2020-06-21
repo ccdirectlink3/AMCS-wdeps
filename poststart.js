1cd47385ab331080a60b0382e4a61cf02d7d7cc8
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-06-21 18:13:51 +0300

    upgrade to ccloader 3

--- poststart.js
+++
@@ -1,6 +0,0 @@
-ig.lang.labels.sc.gui.options.headers['AMCS'] = 'AMCS';
-const keyNames = ig.lang.labels.sc.gui.options.controls.keys;
-keyNames['Call'] = 'Call';
-keyNames['Vanish'] = 'Vanish';
-keyNames['Luna dial'] = 'Luna dial';
-keyNames['Reset time flow'] = 'Reset time flow';
